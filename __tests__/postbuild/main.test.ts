78a5f8176d064a308bd8b77074ec11ef7ca5748e
Kenichi Kamiya <kachick1@gmail.com>
2023-01-17 12:28:46 +0900

    Test with final product instead of intermediate file

--- __tests__/postbuild/main.test.ts
+++ __tests__/postbuild/main.test.ts
@@ -12,7 +12,7 @@ test('runs', () => {
   env['GITHUB_REPOSITORY'] = 'kachick/wait-other-jobs';
   env['GITHUB_RUN_ID'] = '2408217435';
   const np = execPath;
-  const ip = join(__dirname, '../..', 'lib', 'main.js');
+  const ip = join(__dirname, '../..', 'dist', 'index.js');
   const options: ExecFileSyncOptions = {
     env,
   };
