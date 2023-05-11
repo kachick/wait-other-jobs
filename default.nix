c51bbdf7928f18452c2355e9ffcf94a586c09885
Kenichi Kamiya <kachick1@gmail.com>
2023-05-11 21:23:48 +0900

    Bump nixpkgs to use latest dprint

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4c3edba85629ec304b5269e4b0ac7f26c433df23.tar.gz") { } }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4ef0dd85324fca49bf06fd9f2d52711503b1128c.tar.gz") { } }:
 
 pkgs.mkShell {
   buildInputs = [
