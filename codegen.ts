cdc14a45e2581a8a1c8d8b3c9b30b617afc7ddcb
Kenichi Kamiya <kachick1@gmail.com>
2023-05-15 19:05:25 +0900

    I was wrong, we need request instead of server
    https://techlife.cookpad.com/entry/2021/03/24/123214

--- codegen.ts
+++ codegen.ts
@@ -1,11 +1,14 @@
+
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
   schema: "https://docs.github.com/public/schema.docs.graphql",
+  documents: "src/**/*.tsx",
   generates: {
-    "src/generated/graphql.ts": {
-      plugins: ["typescript", "typescript-resolvers"]
+    "src/gql/": {
+      preset: "client",
+      plugins: []
     },
     "./graphql.schema.json": {
       plugins: ["introspection"]
