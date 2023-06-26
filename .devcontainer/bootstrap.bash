ed5a3ae496bf6195bb586e3cb1e4e671a9515d85
Kenichi Kamiya <kachick1@gmail.com>
2023-06-27 01:17:48 +0900

    Add debug log

--- .devcontainer/bootstrap.bash
+++ .devcontainer/bootstrap.bash
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-nix-shell --run 'direnv allow && makers setup' && zsh
+whoami && ls -alh /nix && \
+  mkdir -m 0755 -p /nix/var/nix/{profiles,gcroots}/per-user/vscode && \
+  nix-shell --run 'direnv allow && makers setup' && \
+  zsh
