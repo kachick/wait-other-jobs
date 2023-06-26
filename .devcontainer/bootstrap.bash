af54fc382a11eed22e590f6a410fd843f72230e8
Kenichi Kamiya <kachick1@gmail.com>
2023-06-27 01:41:11 +0900

    We drop the mkdir phase again

--- .devcontainer/bootstrap.bash
+++ .devcontainer/bootstrap.bash
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 whoami && ls -alh /nix && \
-  mkdir -m 0755 -p /nix/var/nix/{profiles,gcroots}/per-user/vscode && \
   nix-shell --run 'direnv allow && makers setup' && \
   zsh
