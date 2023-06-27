2a63dd7b121e90367144a5305602a72ea6cea300
Kenichi Kamiya <kachick1@gmail.com>
2023-06-27 18:21:43 +0900

    Fix missing commit

--- .devcontainer/bootstrap.bash
+++ .devcontainer/bootstrap.bash
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-
 whoami && id && cat /etc/passwd && ls -alh /nix && ls -alh . && \
-  nix-shell --run 'direnv allow && makers setup' && \
-  zsh
+  nix-shell --run 'direnv allow && makers setup'
