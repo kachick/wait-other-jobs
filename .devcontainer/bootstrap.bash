c9d9b1bbd909e304fab517805a223e071d9cb711
Kenichi Kamiya <kachick1@gmail.com>
2023-06-28 00:35:01 +0900

    Remove debug code
    Now /etc/passwd does not include actual one, especially in container
    But that makes me afraid

--- .devcontainer/bootstrap.bash
+++ .devcontainer/bootstrap.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-whoami && id && cat /etc/passwd && ls -alh /nix && ls -alh . && \
+whoami && id && ls -alh /nix && ls -alh . && \
   nix-shell --run 'direnv allow && makers setup'
