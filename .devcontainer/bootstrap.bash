aa0b4b6351a86ecff86ee0c6f7b7dc2983606383
Kenichi Kamiya <kachick1@gmail.com>
2023-07-20 01:01:32 +0900

    Fix entry point

--- .devcontainer/bootstrap.bash
+++ .devcontainer/bootstrap.bash
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 whoami && id && ls -alh /nix && ls -alh . && \
-  nix develop --command direnv allow && nix develop
+  nix develop --command direnv allow
+
+if ! (echo "$CI" | grep --silent -F true); then
+  nix develop
+fi
