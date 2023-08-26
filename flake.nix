5149bca61d80355e510c7cb180044704be0c9473
Kenichi Kamiya <kachick1@gmail.com>
2023-08-26 17:39:50 +0900

    Update flake settings

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/21061e4e4a0f46a6f487c7583be916e4e031ee4e";
+    # Candidate channels
+    #   - https://github.com/kachick/anylang-template/issues/17
+    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
+    # How to update the revision
+    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -13,7 +18,7 @@
             inherit system;
             config = {
               permittedInsecurePackages = [
-                "nodejs-16.20.1"
+                "nodejs-16.20.2"
               ];
             };
           };
@@ -22,6 +27,9 @@
         devShells.default = with pkgs;
           mkShell {
             buildInputs = [
+              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+              bashInteractive
+
               nodejs-16_x
               deno
               dprint
