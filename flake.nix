169cded83a3f959df4cad3d003cc7c811c263ca1
Kenichi Kamiya <kachick1@gmail.com>
2023-08-28 13:29:28 +0900

    Bump dependencies with the nixpkgs (#559)
    * Update flake settings
    
    * flake.lock: Update
    
    Flake lock file updates:
    
    • Updated input 'flake-utils':
        'github:numtide/flake-utils/919d646de7be200f3bf08cb76ae1f09402b6f9b4' (2023-07-11)
      → 'github:numtide/flake-utils/f9e7cf818399d17d347f847525c5a5a8032e4e44' (2023-08-23)
    • Updated input 'nixpkgs':
        'github:NixOS/nixpkgs/21061e4e4a0f46a6f487c7583be916e4e031ee4e' (2023-07-19)
      → 'github:NixOS/nixpkgs/18324978d632ffc55ef1d928e81630c620f4f447' (2023-08-24)
    
    * Pin stable nixpkgs for hadolint
    
    * Update CI dependencies with flake versions
    
    * Bump dprint.json with old dprint
    
    `rtx exec dprint@0.39.1 -- dprint config update --yes && rtx exec dprint@0.40.2 -- dprint check`
    
    And removed the outdated includes specifier
    
    * Add comment

--- flake.nix
+++ flake.nix
@@ -1,27 +1,37 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/21061e4e4a0f46a6f487c7583be916e4e031ee4e";
+    # Candidate channels
+    #   - https://github.com/kachick/anylang-template/issues/17
+    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
+    # How to update the revision
+    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs-unstable, nixpkgs-stable, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         # https://discourse.nixos.org/t/mark-a-devshell-dependency-as-insecure/24354/3
-        pkgs = import nixpkgs
+        unstable-pkgs = import nixpkgs-unstable
           {
             inherit system;
             config = {
               permittedInsecurePackages = [
-                "nodejs-16.20.1"
+                "nodejs-16.20.2"
               ];
             };
           };
+        stable-pkgs = nixpkgs-stable.legacyPackages.${system};
       in
       {
-        devShells.default = with pkgs;
+        devShells.default = with unstable-pkgs;
           mkShell {
             buildInputs = [
+              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+              bashInteractive
+
               nodejs-16_x
               deno
               dprint
@@ -30,7 +40,9 @@
               nixpkgs-fmt
               typos
               actionlint
-              hadolint
+              # Avoided broken hadolint in latest
+              # https://github.com/NixOS/nixpkgs/pull/240387#issuecomment-1686601267
+              stable-pkgs.hadolint
             ];
           };
       });
