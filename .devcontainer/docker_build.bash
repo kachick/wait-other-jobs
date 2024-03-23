99b0c8c1ac9e4b5f5670b448e0da4e2be0596ed2
Kenichi Kamiya <kachick1@gmail.com>
2024-03-23 14:48:10 +0900

    Drop unused devcontainer (#735)
    Revert #517
    
    - Now I'm using podman, I can't quickly test the docker based devcontainer stack
    - CI took longtime
    - AFAIK, there is no devcontainer user in contributors. If not, please tell me 🙏

--- .devcontainer/docker_build.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker build . --tag wait-other-jobs:latest --build-arg container_user_uid="$(stat -c '%u' .)" --build-arg container_user_gid="$(stat -c '%g' .)" --file .devcontainer/Dockerfile
--- .devcontainer/docker_shell.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --interactive --tty --rm --volume "$(pwd):/workspaces/wait-other-jobs" --workdir "/workspaces/wait-other-jobs" wait-other-jobs:latest
