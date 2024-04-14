e3cf96a968ce11a4a455c954881c33ce4088a910
Kenichi Kamiya <kachick1@gmail.com>
2024-04-15 02:13:07 +0900

    Add more schema tests

--- src/schema.test.ts
+++ src/schema.test.ts
@@ -1,6 +1,8 @@
 import test from 'node:test';
 import assert from 'node:assert';
 import { Options } from './schema.ts';
+import { optional } from 'zod';
+import { wait } from './wait.ts';
 
 const defaultOptions = Object.freeze({
   isEarlyExit: true,
@@ -14,7 +16,7 @@ const defaultOptions = Object.freeze({
   isDryRun: false,
 });
 
-test('Options keeps given values', () => {
+test('Options keep given values', () => {
   assert.deepStrictEqual({
     isEarlyExit: true,
     attemptLimits: 1000,
@@ -26,6 +28,13 @@ test('Options keeps given values', () => {
     shouldSkipSameWorkflow: false,
     isDryRun: false,
   }, Options.parse(defaultOptions));
+});
+
+test('Options set some default values it cannot be defined in action.yml', () => {
+  assert.deepStrictEqual({
+    ...defaultOptions,
+    waitList: [{ workflowFile: 'ci.yml', optional: false }],
+  }, Options.parse({ ...defaultOptions, waitList: [{ workflowFile: 'ci.yml' }] }));
 });
 
 test('Options reject invalid values', () => {
@@ -42,5 +51,15 @@ test('Options reject invalid values', () => {
   assert.throws(() => Options.parse({ ...defaultOptions, retryMethod: 'inverse-exponential-backoff' }), {
     name: 'ZodError',
     message: /invalid_enum_value/,
+  });
+
+  assert.throws(() => Options.parse({ ...defaultOptions, waitList: [{ unknownField: ':)' }] }), {
+    name: 'ZodError',
+    message: /invalid_type/,
+  });
+
+  assert.throws(() => Options.parse({ ...defaultOptions, skipList: [{ optional: true }] }), {
+    name: 'ZodError',
+    message: /invalid_type/,
   });
 });
