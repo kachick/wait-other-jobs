c24381c6208d0092e309d0dbf24ca7f838e76285
Kenichi Kamiya <kachick1@gmail.com>
2024-05-30 04:00:02 +0900

    Clarify same triggers

--- src/report.test.ts
+++ src/report.test.ts
@@ -50,124 +50,113 @@ test('wait-list', async (t) => {
     }, omit<Report, 'summaries'>(report, ['summaries']));
   });
 
-  await t.test('expect required slowstarting job and set enough startupGracePeriod', (_t) => {
-    const report = generateReport(
-      checks92810686811WaitSuccessPolling1,
-      {
-        owner: 'kachick',
-        repo: 'wait-other-jobs',
-        'runId': 92810686811,
-        ref: '8c14d2a44d6dff4e69b0a3cacc2a14e416b44137',
-        jobName: 'wait-success',
-        eventName: 'pull_request',
-      },
-      Temporal.Duration.from({ milliseconds: Math.ceil(986.9570700004697) }),
-      {
-        'waitList': [
-          {
-            'workflowFile': 'GH-820-graceperiod.yml',
-            'jobName': 'quickstarter-success',
-            'optional': false,
-            'startupGracePeriod': { seconds: 10 },
-          },
-          {
-            'workflowFile': 'GH-820-graceperiod.yml',
-            'jobName': 'slowstarter-success',
-            'optional': false,
-            'startupGracePeriod': { seconds: 60 },
-          },
-        ],
-        skipList: [],
-        shouldSkipSameWorkflow: false,
-      },
-    );
+  await t.test('startupGracePeriod', async (t) => {
+    const trigger = Object.freeze({
+      owner: 'kachick',
+      repo: 'wait-other-jobs',
+      'runId': 92810686811,
+      ref: '8c14d2a44d6dff4e69b0a3cacc2a14e416b44137',
+      jobName: 'wait-success',
+      eventName: 'pull_request',
+    });
+    await t.test('required slowstarting job and set enough grace period', (_t) => {
+      const report = generateReport(
+        checks92810686811WaitSuccessPolling1,
+        trigger,
+        Temporal.Duration.from({ milliseconds: Math.ceil(986.9570700004697) }),
+        {
+          'waitList': [
+            {
+              'workflowFile': 'GH-820-graceperiod.yml',
+              'jobName': 'quickstarter-success',
+              'optional': false,
+              'startupGracePeriod': { seconds: 10 },
+            },
+            {
+              'workflowFile': 'GH-820-graceperiod.yml',
+              'jobName': 'slowstarter-success',
+              'optional': false,
+              'startupGracePeriod': { seconds: 60 },
+            },
+          ],
+          skipList: [],
+          shouldSkipSameWorkflow: false,
+        },
+      );
 
-    assert.deepEqual({
-      conclusion: 'acceptable',
-      progress: 'in_progress',
-      description:
-        'Some expected jobs were not started: [{"workflowFile":"GH-820-graceperiod.yml","jobName":"slowstarter-success","optional":false,"startupGracePeriod":{"seconds":60},"found":false}]',
-    }, omit<Report, 'summaries'>(report, ['summaries']));
-  });
+      assert.deepEqual({
+        conclusion: 'acceptable',
+        progress: 'in_progress',
+        description:
+          'Some expected jobs were not started: [{"workflowFile":"GH-820-graceperiod.yml","jobName":"slowstarter-success","optional":false,"startupGracePeriod":{"seconds":60},"found":false}]',
+      }, omit<Report, 'summaries'>(report, ['summaries']));
+    });
 
-  await t.test('slowstarting job has been expired to the GracePeriod', (_t) => {
-    const report = generateReport(
-      checks92810686811WaitSuccessPolling1,
-      {
-        owner: 'kachick',
-        repo: 'wait-other-jobs',
-        'runId': 92810686811,
-        ref: '8c14d2a44d6dff4e69b0a3cacc2a14e416b44137',
-        jobName: 'wait-success',
-        eventName: 'pull_request',
-      },
-      Temporal.Duration.from({ seconds: 60, milliseconds: 1 }),
-      {
-        'waitList': [
-          {
-            'workflowFile': 'GH-820-graceperiod.yml',
-            'jobName': 'quickstarter-success',
-            'optional': false,
-            'startupGracePeriod': { seconds: 10 },
-          },
-          {
-            'workflowFile': 'GH-820-graceperiod.yml',
-            'jobName': 'slowstarter-success',
-            'optional': false,
-            'startupGracePeriod': { seconds: 60 },
-          },
-        ],
-        skipList: [],
-        shouldSkipSameWorkflow: false,
-      },
-    );
+    await t.test('slowstarting job has been expired to the given period', (_t) => {
+      const report = generateReport(
+        checks92810686811WaitSuccessPolling1,
+        trigger,
+        Temporal.Duration.from({ seconds: 60, milliseconds: 1 }),
+        {
+          'waitList': [
+            {
+              'workflowFile': 'GH-820-graceperiod.yml',
+              'jobName': 'quickstarter-success',
+              'optional': false,
+              'startupGracePeriod': { seconds: 10 },
+            },
+            {
+              'workflowFile': 'GH-820-graceperiod.yml',
+              'jobName': 'slowstarter-success',
+              'optional': false,
+              'startupGracePeriod': { seconds: 60 },
+            },
+          ],
+          skipList: [],
+          shouldSkipSameWorkflow: false,
+        },
+      );
 
-    assert.deepEqual({
-      conclusion: 'bad',
-      progress: 'in_progress',
-      description:
-        'Failed to meet some runs on your specified wait-list: [{"workflowFile":"GH-820-graceperiod.yml","jobName":"slowstarter-success","optional":false,"startupGracePeriod":{"seconds":60},"found":false}]',
-    }, omit<Report, 'summaries'>(report, ['summaries']));
-  });
+      assert.deepEqual({
+        conclusion: 'bad',
+        progress: 'in_progress',
+        description:
+          'Failed to meet some runs on your specified wait-list: [{"workflowFile":"GH-820-graceperiod.yml","jobName":"slowstarter-success","optional":false,"startupGracePeriod":{"seconds":60},"found":false}]',
+      }, omit<Report, 'summaries'>(report, ['summaries']));
+    });
 
-  await t.test('GracePeriod judges as expired for same durations', (_t) => {
-    const report = generateReport(
-      checks92810686811WaitSuccessPolling1,
-      {
-        owner: 'kachick',
-        repo: 'wait-other-jobs',
-        'runId': 92810686811,
-        ref: '8c14d2a44d6dff4e69b0a3cacc2a14e416b44137',
-        jobName: 'wait-success',
-        eventName: 'pull_request',
-      },
-      Temporal.Duration.from({ seconds: 60 }),
-      {
-        'waitList': [
-          {
-            'workflowFile': 'GH-820-graceperiod.yml',
-            'jobName': 'quickstarter-success',
-            'optional': false,
-            'startupGracePeriod': { seconds: 10 },
-          },
-          {
-            'workflowFile': 'GH-820-graceperiod.yml',
-            'jobName': 'slowstarter-success',
-            'optional': false,
-            'startupGracePeriod': { seconds: 60 },
-          },
-        ],
-        skipList: [],
-        shouldSkipSameWorkflow: false,
-      },
-    );
+    await t.test('judges as expired for same durations', (_t) => {
+      const report = generateReport(
+        checks92810686811WaitSuccessPolling1,
+        trigger,
+        Temporal.Duration.from({ seconds: 60 }),
+        {
+          'waitList': [
+            {
+              'workflowFile': 'GH-820-graceperiod.yml',
+              'jobName': 'quickstarter-success',
+              'optional': false,
+              'startupGracePeriod': { seconds: 10 },
+            },
+            {
+              'workflowFile': 'GH-820-graceperiod.yml',
+              'jobName': 'slowstarter-success',
+              'optional': false,
+              'startupGracePeriod': { seconds: 60 },
+            },
+          ],
+          skipList: [],
+          shouldSkipSameWorkflow: false,
+        },
+      );
 
-    assert.deepEqual({
-      conclusion: 'bad',
-      progress: 'in_progress',
-      description:
-        'Failed to meet some runs on your specified wait-list: [{"workflowFile":"GH-820-graceperiod.yml","jobName":"slowstarter-success","optional":false,"startupGracePeriod":{"seconds":60},"found":false}]',
-    }, omit<Report, 'summaries'>(report, ['summaries']));
+      assert.deepEqual({
+        conclusion: 'bad',
+        progress: 'in_progress',
+        description:
+          'Failed to meet some runs on your specified wait-list: [{"workflowFile":"GH-820-graceperiod.yml","jobName":"slowstarter-success","optional":false,"startupGracePeriod":{"seconds":60},"found":false}]',
+      }, omit<Report, 'summaries'>(report, ['summaries']));
+    });
   });
 });
 
