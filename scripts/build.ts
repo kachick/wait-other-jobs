aff71f6e658cd9854e65e42df5bc74fa56a12bb6
Kenichi Kamiya <kachick1@gmail.com>
2024-09-12 03:55:19 +0900

    Remove needless indent in esbuild workaround

--- scripts/build.ts
+++ scripts/build.ts
@@ -10,13 +10,12 @@ await esbuild.build({
   target: 'node20',
   platform: 'node',
   banner: {
-    js: `
-    import path from 'path';
-    import { fileURLToPath } from 'url';
-    import { createRequire as topLevelCreateRequire } from 'module';
-    const require = topLevelCreateRequire(import.meta.url);
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-        `,
+    js: `import path from 'path';
+import { fileURLToPath } from 'url';
+import { createRequire as topLevelCreateRequire } from 'module';
+const require = topLevelCreateRequire(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+`,
   },
 });
