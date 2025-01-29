2cc42e68a1ddf8fa5242a79f0facda558309706a
Kenichi Kamiya <kachick1@gmail.com>
2025-01-29 20:23:18 +0900

    This comment is still valid even after using CLI

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S deno run --allow-all
 import $ from 'jsr:@david/dax';
 
+// https://github.com/evanw/esbuild/issues/1921#issuecomment-1491470829
+// https://github.com/evanw/esbuild/issues/1921#issuecomment-1575636282
 const text = await Deno.readTextFile('dependencies/banner.js');
 await $`esbuild --bundle 'src/main.ts' --outfile='dist/index.js' --format=esm --target=node20 --platform=node --banner:js=${text}`;
