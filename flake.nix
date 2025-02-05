2924faeea276c707ad8163f6dd7f67a780569070
Kenichi Kamiya <kachick1@gmail.com>
2025-02-05 21:56:15 +0900

    Rewrite sync tasks with nix devshell

--- flake.nix
+++ flake.nix
@@ -1,7 +1,5 @@
 {
   inputs = {
-    # How to update the revision
-    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
@@ -54,45 +52,6 @@
                 gitleaks
               ]
             );
-          };
-        }
-      );
-
-      apps = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          bump-nix-dependencies = {
-            type = "app";
-            program =
-              with pkgs;
-              lib.getExe (writeShellApplication {
-                name = "bump-nix-dependencies.bash";
-                runtimeInputs = [
-                  nix
-                  git
-                  sd # TODO: Replace with something
-                  nodejs_20
-                  nodejs_20.pkgs.pnpm
-                ];
-                # Why --really-refresh?: https://stackoverflow.com/q/34807971
-                text = ''
-                  set -x
-
-                  node --version | sd '^v?' "" > .node-version && git add .node-version
-                  git update-index -q --really-refresh
-                  git diff-index --quiet HEAD || git commit -m 'Sync .node-version with nixpkgs' .node-version
-
-                  sd '("packageManager": "pnpm)@([0-9\.]+)' "\$1@$(pnpm --version)" package.json && git add package.json
-                  git update-index -q --really-refresh
-                  git diff-index --quiet HEAD || git commit -m 'Sync pnpm version with nixpkgs' package.json
-                '';
-                meta = {
-                  description = "Bump dependency versions except managed by node package manager";
-                };
-              });
           };
         }
       );
