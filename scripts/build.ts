2391257ed3d67a09d2344ac20fb3ceb4771d0ba8
Kenichi Kamiya <kachick1@gmail.com>
2025-02-07 17:26:06 +0900

    Remove unnecessary await

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,5 +3,5 @@ import $ from 'jsr:@david/dax';
 
 // https://github.com/evanw/esbuild/issues/1921#issuecomment-1491470829
 // https://github.com/evanw/esbuild/issues/1921#issuecomment-1575636282
-const text = await Deno.readTextFile('dependencies/banner.js');
+const text = Deno.readTextFileSync('dependencies/banner.js');
 await $`esbuild --bundle 'src/main.ts' --outfile='dist/index.js' --format=esm --target=node20 --platform=node --banner:js=${text}`;
