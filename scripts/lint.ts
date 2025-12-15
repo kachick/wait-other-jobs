734e111771a491ecc243da2942fa1798255f3d49
Kenichi Kamiya <kachick1@gmail.com>
2025-12-16 01:17:29 +0900

    Replace deno_lint with biome

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -3,7 +3,7 @@ import $ from 'jsr:@david/dax@^0.43.1';
 
 await Promise.all([
   $`dprint check`,
-  $`deno lint`,
+  $`biome check`,
   $`typos . .github .vscode`,
   $`gitleaks dir .`, // git mode is 4x slower
   $`git ls-files '*.nix' | xargs nixfmt --check`,
